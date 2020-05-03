928e80ff2effc44a82bd60e313b1169622aa4a15
kshivam213 <kshivam213@gmail.com>
2020-05-04 03:04:06 +0530

    Changed the class name

--- src/week1/RansomNote_Day3.java
+++ src/week1/RansomNote_Day3.java
@@ -3,11 +3,25 @@ package week1;
 import java.util.HashMap;
 import java.util.Map;
 
-public class Day3Challenge {
+/*
+ * Problem Description
+ * Given an arbitrary ransom note string and another string containing letters from all the magazines, write a function that will return true if the ransom note can be constructed from the magazines ; otherwise, it will return false.
+
+   Each letter in the magazine string can only be used once in your ransom note.
+
+   Note:
+   You may assume that both strings contain only lowercase letters.
+
+   canConstruct("a", "b") -> false
+   canConstruct("aa", "ab") -> false
+   canConstruct("aa", "aab") -> true
+*/
+
+public class RansomNote_Day3 {
 		
 	public static void main(String args[]) {
 		Solution sol = new Solution();
-		System.out.println(sol.canConstruct("a","b"));
+		System.out.println(sol.canConstruct("aab","bbbacaa"));
 	}
 }
 
